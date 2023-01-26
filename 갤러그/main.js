b79e60cd3cd153e477cddcc0542e496f420b6bc1
leehy98 <117696170+leehy98@users.noreply.github.com>
2023-01-26 18:22:16 +0900

    Add files via upload
    우주선 움직이는거 까지 했음

--- 갤러그/main.js
+++ 갤러그/main.js
@@ -1,24 +1,28 @@
 
-
+// canvas setting
 let canvas;
 let ctx;
 canvas = document.createElement("canvas");
-canvas.width = 1200;
-canvas.height = 700;
+canvas.width = 1400;
+canvas.height = 900;
 ctx = canvas.getContext("2d");
 document.body.appendChild(canvas);
 
+let spaceShipHealth;
+
+
+
 let backgroundImage,shipImage,bulletImage,enemyImage,middleEnemyImage,BossImage;
+let spaceShipX = canvas.width*0.45;
+let spaceShipY = canvas.height*0.85;
 
-
-
-
+// loadImage function
 function loadImage(){
     backgroundImage = new Image();
     backgroundImage.src = "image/background.png";
 
     shipImage = new Image();
-    shipImage.src = "image/spaceShip.png";
+    shipImage.src = "image/spaceShip .png";
 
     bulletImage = new Image();
     bulletImage.src = "image/bullet.png";
@@ -33,14 +37,37 @@ function loadImage(){
     BossImage.src = "image/BOSS.png";
 }
 
-function render(){
-    ctx.drawImage(backgroundImage,0,0,canvas.width,canvas.height);
+let keydown
+function setKeyBoardListener(){
+    document.addEventListener("keydown",function(event){
+         case 39:
+                    spaceShipX("left",1);
+                        break;
+                case 37:
+                     spaceShipX("left",-1);
+                        break;
+                case 40:
+                    spaceShipY("top",  1);
+                        break;
+                case 38 :
+                    spaceShipY("top, -1");
+                        break;
+    });
 
 }
+// render function
+function render(){
+    ctx.drawImage(backgroundImage,0,0,canvas.width,canvas.height);
+    ctx.drawImage(shipImage,spaceShipX,spaceShipY);
+}
 
+//main function
 function main(){
     render()
     requestAnimationFrame(main)
 }
+
+// call function
 loadImage();
+setKeyBoardListener();
 main();
